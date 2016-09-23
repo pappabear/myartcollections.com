6b408197a84afa689b83aa56348cac301b41ef6d
Chip Irek <chip.irek@gmail.com>
2016-09-23 14:39:03 -0400

    Just enough user scaffolding to test if Elastic Beanstalk will like this configuration.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = APP_NAME
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
