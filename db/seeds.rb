1b5e155c9aa3ef56d890924e36f7694e653872e7
Chip Irek <chip.irek@gmail.com>
2016-09-24 13:01:15 -0400

    Adding skin from PaperKit pro

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+User.create(name: "Chip Irek", email: "chip.irek@gmail.com", password: "lollip0p", password_confirmation: "lollip0p",
+            activated: true,
+            activated_at: Time.zone.now)
+
+User.create(name: "Michael Hartl", email: "mhartl@example.com", password: "foobar", password_confirmation: "foobar",
+            activated: true,
+            activated_at: Time.zone.now)
+
