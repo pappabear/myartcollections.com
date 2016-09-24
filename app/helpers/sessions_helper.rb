6b0c8968b4b8ab4f4bfd93f64b5c6a906efbe908
Chip Irek <chip.irek@gmail.com>
2016-09-24 09:21:00 -0400

    Adding password resets, account activation, and static pages

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,22 @@
 module SessionsHelper
-
+  
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
+  
   # Returns true if the given user is the current user.
   def current_user?(user)
     user == current_user
   end
-
+  
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -29,36 +29,47 @@ module SessionsHelper
       end
     end
   end
-
+  
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
-
+  
   # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-
+  
   # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-
+  
   # Redirects to stored location (or to the default).
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
   end
-
+  
   # Stores the URL trying to be accessed.
   def store_location
     session[:forwarding_url] = request.url if request.get?
   end
 
+
+  # returns true if the current user is a site admin
+  def is_admin?
+    r = false
+    if current_user
+      if current_user.admin == true
+        r = true
+      end
+    end
+    return r
+  end
 
 end
