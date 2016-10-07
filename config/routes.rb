b177281994624c081aed95d133af00330ec1640b
Chip Irek <chip.irek@gmail.com>
2016-10-07 14:19:30 -0400

    Added an explicit route for 'home' for now -- until i need something different later

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root                'collections#index'
+  get    'home'    => 'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
